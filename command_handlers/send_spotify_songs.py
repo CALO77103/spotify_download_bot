cfece98b45ae0139167ffc0d1b38621d20c14b37
CALO77103 <71672614+CALO77103@users.noreply.github.com>
2021-06-02 16:44:00 +0200

    Update send_spotify_songs.py

--- command_handlers/send_spotify_songs.py
+++ command_handlers/send_spotify_songs.py
@@ -11,10 +11,10 @@ def send_spotify_songs(update: Update, context: CallbackContext):
     download_path = os.getcwd() + "/" + str(uuid.uuid4())
 
     context.bot.send_message(
-        chat_id=update.effective_chat.id, text=f"downloading songs..."
+        chat_id=update.effective_chat.id, text=f"I'm downloading this song, please wait a while"
     )
 
     spotdl.download_from_spotify(download_path, song_link)
     spotdl.send_songs_from_directory(download_path, update, context)
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text="done")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="finished! If u can't see this please retry again or contact support here @SpotifySupportbot")
